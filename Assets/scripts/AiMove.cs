ef095e5fc66ac350782eb4325db47bb31fc2c573
Jin Soo Kang <iidumbb@Jins-iMac.local>
2018-08-06 13:57:44 -0500

    first commit

---
+++ Assets/scripts/AiMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AiMove : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public Transform[] moveSpot;
+    public int randomMoveSpot;
+ 
+
+	// Use this for initialization
+	void Start () {
+        if(agent == null){
+
+            Debug.Log("Agent is null.");
+        }else{
+            //setDestination();
+        }
+
+        randomMoveSpot = Random.Range(0, moveSpot.Length);
+
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        agent.SetDestination(moveSpot[randomMoveSpot].position);
+
+        if(transform.position == moveSpot[randomMoveSpot].position){
+            Debug.Log("I am here");
+        }
+	}
+
+    void setDestination(){
+
+       // agent.SetDestination(moveSpot.position);
+    }
+}
