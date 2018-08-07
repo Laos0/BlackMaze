e55c5d0c5abb10daa629427ff80493ce26fad478
C-EC8EB5A41BCA\Student <isonylao@gmail.com>
2018-08-06 23:56:22 -0500

    throwing items

--- Assets/scripts/Inventory.cs
+++ Assets/scripts/Inventory.cs
@@ -7,7 +7,11 @@ public class Inventory : MonoBehaviour {
     // public Image[] imageArray;
    // List<LightType> lights;
     public LightType[] lights;
-    
+    public GameObject candle;
+    public GameObject torch;
+    public GameObject lantern;
+    public GameObject lightCon;
+
 
     public int activeIndex;
     public int slotCounter;
@@ -37,9 +41,37 @@ public class Inventory : MonoBehaviour {
         return false;
     }
 
-    public void removeActiveLight()
+    public GameObject removeActiveLight()
     {
         //inven[activeIndex];
+       LightType light = lights[activeIndex];
+        lights[activeIndex] = LightType.NONE;
+        GameObject lightGO = null;
+
+        if (light == LightType.CANDLE)
+        {
+            lightGO = Instantiate(candle);
+                
+        }
+        else if (light == LightType.LANTERN)
+        {
+            lightGO = Instantiate(lantern, transform);
+                
+        }
+        else if (light == LightType.TORCH)
+        {
+            lightGO = Instantiate(torch, transform);
+                
+        }
+
+        if(lightGO != null)
+        {
+            lightGO.transform.SetParent(lightCon.transform);
+        }
+
+        return lightGO;
+        
+
     }
 
 
---
+++ Assets/scripts/ThrowItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowItem : MonoBehaviour {
+
+    public Inventory inventory;
+   
+
+    public int counter;
+
+    private void Start()
+    {
+        counter = 0;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            GameObject lightGO = inventory.removeActiveLight();
+            if(lightGO != null)
+            {
+                lightGO.GetComponent<Rigidbody>().AddForce(transform.forward * 100);
+            }
+        }
+    }
+
+
+}
